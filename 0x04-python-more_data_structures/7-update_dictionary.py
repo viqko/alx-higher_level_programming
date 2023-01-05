8734b956a693571eb0e89f2e1a0147deb9c75e7c
Victor Tettey TETTEH <tvictortetteh@gmail.com>
2023-01-05 03:34:02 +0000

    Create 7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    A function that replaces or adds
+    key/value in a dictionary.
+    """
+    a_dictionary[key] = value
+    return a_dictionary
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
