bc1ddbc071ad15e5945d69ada82489c713969506
Victor Tettey TETTEH <tvictortetteh@gmail.com>
2023-01-24 16:20:32 +0100

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def islower(c):
-    """Print a string in lowercase."""
-    if ord(c) >= 97 and ord(c) <= 122:
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
-    else:
-        return False
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
