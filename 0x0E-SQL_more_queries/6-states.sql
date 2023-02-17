c2a45c088cfba35f4391dc513d413daf950584a0
Victor Tettey TETTEH <tvictortetteh@gmail.com>
2023-02-17 14:57:52 +0100

    Create 6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- A script that creates the database hbtn_0d_usa and the table states
+-- in the database hbtn_0d_usa 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+	);
